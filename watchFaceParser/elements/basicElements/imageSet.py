ebd871f1e7b08b47563a170bcf7eba3b8fe9bcc3
Nikolas Maletschkin <tnxec2@gmail.com>
2022-05-31 16:59:29 +0200

    unknown5 und 6

--- watchFaceParser/elements/basicElements/imageSet.py
+++ watchFaceParser/elements/basicElements/imageSet.py
@@ -4,4 +4,6 @@
         2: { 'Name': 'Y', 'Type': 'long'},
         3: { 'Name': 'ImageIndex', 'Type': 'long'},
         4: { 'Name': 'ImagesCount', 'Type': 'long'},
+        5: { 'Name': 'Unknown5', 'Type': 'long'},
+        6: { 'Name': 'Unknown6', 'Type': 'long'},
     }
