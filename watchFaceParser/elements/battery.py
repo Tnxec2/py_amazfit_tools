3082c9cdc3ce40de08f15e5f90771052cf84da2e
Nikolas Maletschkin <tnxec2@gmail.com>
2022-06-15 07:22:51 +0200

    battery lineariconset

--- watchFaceParser/elements/battery.py
+++ watchFaceParser/elements/battery.py
@@ -4,13 +4,14 @@ from watchFaceParser.elements.basicElements.image import Image
 from watchFaceParser.elements.basicElements.imageSet import ImageSet
 from watchFaceParser.elements.basicElements.iconSet import IconSet
 
+from watchFaceParser.elements.basicElements.linearIconSet import LinearIconSet
 
 
 class Battery:
     definitions = {
         1: { 'Name': 'Text', 'Type': BatteryNumber},
         2: { 'Name': 'Icon', 'Type': ImageSet}, 
-        3: { 'Name': 'Scale', 'Type': IconSet}, 
+        3: { 'Name': 'Scale', 'Type': LinearIconSet}, 
         4: { 'Name': 'Circle', 'Type': CircleScale}, 
         5: { 'Name': 'Unknown5', 'Type': 'long?'}, 
         6: { 'Name': 'Unknown6', 'Type': 'long?'}, 
