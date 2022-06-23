e8adbc48050ad3768993aab6d3bab51152feb95b
Nikolas Maletschkin <tnxec2@gmail.com>
2022-06-23 11:44:53 +0200

    fix battery preview

--- watchFaceParser/models/elements/batteryElement.py
+++ watchFaceParser/models/elements/batteryElement.py
@@ -6,12 +6,21 @@ from watchFaceParser.models.elements.basic.containerElement import ContainerElem
 class BatteryElement(ContainerElement):
     def __init__(self, parameter, parent = None, name = None):
         self._text = None
-        self._percent = None
         self._scale = None
         self._icon = None
         self._circle = None
         super(BatteryElement, self).__init__(parameters = None, parameter = parameter, parent = parent, name = name)
 
+    
+    def draw3(self, drawer, images, state):
+        if self._text:
+            self._text.draw3(drawer, images, state)
+        if self._icon:
+            self._icon.draw4(drawer, images, state.getBatteryLevel(), 100)
+        if self._scale:
+            self._scale.draw4(drawer, images, state.getBatteryLevel(), 100)
+        if self._circle:
+            self._circle.draw4(drawer, images, state.getBatteryLevel() , 100)
 
     def createChildForParameter(self, parameter):
         parameterId = parameter.getId()
@@ -21,16 +30,16 @@ class BatteryElement(ContainerElement):
             self._text = BatteryNumberElement(parameter = parameter, parent = self, name = 'Text')
             return self._text
         elif parameterId == 2:
-            from watchFaceParser.models.elements.battery.batteryImageSetElement import BatteryImageSetElement # temp.
-            self._icon = BatteryImageSetElement(parameter = parameter, parent = self, name = 'Icon')
+            from watchFaceParser.models.elements.common.imageSetElement import ImageSetElement # temp.
+            self._icon = ImageSetElement(parameter = parameter, parent = self, name = 'Icon')
             return self._icon
         elif parameterId == 3:
-            from watchFaceParser.models.elements.battery.batteryLinearIconsetElement import BatteryLinearIconSetElement
-            self._scale = BatteryLinearIconSetElement(parameter = parameter, parent = self, name = 'Scale')
+            from watchFaceParser.models.elements.common.linearIconSetElement import LinearIconSetElement
+            self._scale = LinearIconSetElement(parameter = parameter, parent = self, name = 'Scale')
             return self._scale
         elif parameterId == 4:
-            from watchFaceParser.models.elements.battery.batteryCircleProgressElement import BatteryCircleProgressElement
-            self._circle = BatteryCircleProgressElement(parameter = parameter, parent = self, name = 'Circle')
+            from watchFaceParser.models.elements.common.circularProgressElement import CircularProgressElement
+            self._circle = CircularProgressElement(parameter = parameter, parent = self, name = 'Circle')
             return self._circle
         else:
             return super(BatteryElement, self).createChildForParameter(parameter)
