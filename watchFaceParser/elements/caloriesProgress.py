525c329747d903c519c422282aa3cd357de4f94f
Nikolas Maletschkin <tnxec2@gmail.com>
2022-06-24 07:53:35 +0200

    update calories progress

--- watchFaceParser/elements/caloriesProgress.py
+++ watchFaceParser/elements/caloriesProgress.py
@@ -6,9 +6,8 @@ from watchFaceParser.elements.basicElements.linearIconSet import LinearIconSet
 
 class CaloriesProgress:
     definitions = {
-        1: { 'Name': 'GoalImage', 'Type': Image},  # TODO: test on watch
+        1: { 'Name': 'GoalImage', 'Type': Image},  # work on watch
         2: { 'Name': 'Icon', 'Type': ImageSet}, # zepp: ex5urh0a24zK8SgYzj80wOZ0cqB3TX8NIpPVYNHS
-        3: { 'Name': 'Gauge', 'Type': LinearIconSet}, # TODO: test on watch
         4: { 'Name': 'Circle', 'Type': CircleScale}, # zepp: Q45abXyTkaM6IFZdCsUwdseezHZhuZmi70i39r97
     }
 
--- watchFaceParser/models/elements/activity/caloriesProgressElement.py
+++ watchFaceParser/models/elements/activity/caloriesProgressElement.py
@@ -4,22 +4,27 @@ from watchFaceParser.models.elements.basic.containerElement import ContainerElem
 
 class CaloriesProgressElement(ContainerElement):
     def __init__(self, parameter, parent = None, name = None):
+        self._goalimage = None
         self._circle = None
         self._icon = None
         super(CaloriesProgressElement, self).__init__(parameters = None, parameter = parameter, parent = parent, name = name)
 
     def draw3(self, drawer, images, state):
-
+        goal = 200
+        if self._goalimage:
+            if state.getCalories() >= goal:
+                self._goalimage.draw3(drawer, images, state)
         if self._icon:
-            self._icon.draw4(drawer, images, state.getCalories(), 200)
-
+            self._icon.draw4(drawer, images, state.getCalories(), goal)
         if self._circle:
-            self._circle.draw4(drawer, images, state.getCalories(), 200)
+            self._circle.draw4(drawer, images, state.getCalories(), goal)
 
     def createChildForParameter(self, parameter):
         parameterId = parameter.getId()
         if parameterId == 1:
-            pass
+            from watchFaceParser.models.elements.common.imageElement import ImageElement
+            self._goalimage = ImageElement(parameter = parameter, parent = self, name = 'GoalImage')
+            return self._goalimage
         elif parameterId == 2:
             from watchFaceParser.models.elements.common.imageSetElement import ImageSetElement
             self._icon = ImageSetElement(parameter = parameter, parent = self, name = 'Icons')
