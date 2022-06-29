293003fd8f9f808cdc834b2398d8ac9bd375e74f
Nikolas Maletschkin <tnxec2@gmail.com>
2022-06-29 13:08:49 +0200

    aqi icon progress

--- watchFaceParser/elements/weatherElements/aqi.py
+++ watchFaceParser/elements/weatherElements/aqi.py
@@ -1,10 +1,12 @@
 from watchFaceParser.elements.basicElements.number import Number
 from watchFaceParser.elements.basicElements.image import Image
+from watchFaceParser.elements.basicElements.imageSet import ImageSet
 
 
 class AQI:
     definitions = {
         1: { 'Name': 'Number', 'Type': Number},
+        2: { 'Name': 'IconProgress', 'Type': ImageSet},
         4: { 'Name': 'Icon', 'Type': Image},
         5: { 'Name': 'IconCH', 'Type': Image},
         6: { 'Name': 'IconCN', 'Type': Image},
