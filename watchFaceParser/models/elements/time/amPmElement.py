ecbd592c67478634d95adf7f4b086fdcfe22b287
Nikolas Maletschkin <tnxec2@gmail.com>
2022-07-18 16:15:01 +0200

    fix preview ampm

--- watchFaceParser/models/elements/time/amPmElement.py
+++ watchFaceParser/models/elements/time/amPmElement.py
@@ -6,20 +6,16 @@ class AmPmElement(CoordinatesElement):
     def __init__(self, parameter, parent, name = None):
         self._imageIndexAmCn = None
         self._imageIndexPmCn = None
+        self._imageIndexAmEn = None
+        self._imageIndexPmEn = None
         super(AmPmElement, self).__init__(parameter = parameter, parent = parent, name = name)
-
-
-    def getImageIndexAmCn(self):
-        return self._imageIndexAmCn
-
-
-    def getImageIndexPmCn(self):
-        return self._imageIndexPmCn
 
 
     def draw3(self, drawer, resources, state):
         assert(type(resources) == list)
-        imageIndex = self.getImageIndexAmCn() if state.getTime().hour < 12 else self.getImageIndexPmCn()
+        imageIndex = self._imageIndexAmEn if state.getTime().hour < 12 else self._imageIndexPmEn
+        if not imageIndex:
+            imageIndex = self._imageIndexAmCn if state.getTime().hour < 12 else self._imageIndexPmCn
         temp = resources[imageIndex].getBitmap()
         drawer.paste(temp, (self._x, self._y), temp)
 
@@ -29,10 +25,16 @@ class AmPmElement(CoordinatesElement):
         from watchFaceParser.models.elements.basic.valueElement import ValueElement
         if parameterId == 3:
             self._imageIndexAmCn = parameter.getValue()
-            return ValueElement(parameter = parameter, parent = self, name = 'ImageIndexAmCn')
+            return ValueElement(parameter = parameter, parent = self, name = 'ImageIndexAMCN')
         elif parameterId == 4:
             self._imageIndexPmCn = parameter.getValue()
-            return ValueElement(parameter = parameter, parent = self, name = 'ImageIndexPmCn')
+            return ValueElement(parameter = parameter, parent = self, name = 'ImageIndexPMCN')
+        if parameterId == 5:
+            self._imageIndexAmEn = parameter.getValue()
+            return ValueElement(parameter = parameter, parent = self, name = 'ImageIndexAMEN')
+        elif parameterId == 6:
+            self._imageIndexPmEn = parameter.getValue()
+            return ValueElement(parameter = parameter, parent = self, name = 'ImageIndexPMEN')
         else:
             return super(AmPmElement, self).createChildForParameter(parameter)
 
